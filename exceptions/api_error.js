aff92d82a5b74075ed507c379852227ebf193238
Ivan Starchykov <48174604+starchykov@users.noreply.github.com>
2024-03-16 23:37:15 +0200

    Some general improvements

--- exceptions/api_error.js
+++ exceptions/api_error.js
@@ -9,7 +9,7 @@ module.exports = class ApiError extends Error {
     }
 
     static UnauthorizedError() {
-        return new ApiError(401, 'Пользователь не авторизован')
+        return new ApiError(401, 'Unauthorized access')
     }
 
     static BadRequest(message, errors = []) {
